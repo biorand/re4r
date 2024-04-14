2d9b9803629cc624d93b02d0b80f84259c7329eb
Ted John <ted@brambles.org>
2024-04-15 00:41:27 +0100

    Try out svelte

---
+++ src/biorand-re4r-web/src/lib/card.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let title: string;
+</script>
+
+<div class="col-auto">
+  <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      {#if title}
+        <h5 class="card-title">{title}</h5>
+      {/if}
+      {#if $$slots.text}
+        <p class="card-text"><slot name="text" /></p>
+      {/if}
+      {#if $$slots.extra}
+        <slot name="extra" />
+      {/if}
+    </div>
+  </div>
+</div>
---
+++ src/biorand-re4r-web/src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Card from '$lib/card.svelte';
+</script>
+
+<div class="container my-3">
+  <div class="card">
+    <div class="card-body">
+      <div class="row">
+        <h3>Your randomizer is ready!</h3>
+        <p>Download the appropriate file and enjoy!</p>
+      </div>
+      <div class="row">
+        <Card title="Patch">
+          <span slot="text">Simply drop this file into your RE 4 install folder.</span>
+          <span slot="extra"
+            ><a id="btn-download-pak" href="#" class="btn btn-success">Download</a></span
+          >
+        </Card>
+        <Card title="Fluffy Mod">
+          <span slot="text">Add this to Fluffy Mod manager and enable it.</span>
+          <span slot="extra"
+            ><a id="btn-download-mod" href="#" class="btn btn-success">Download</a></span
+          >
+        </Card>
+      </div>
+    </div>
+  </div>
+</div>
