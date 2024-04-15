6c2adc798eef10f40c5a9be715e260d57ee5933d
Ted John <ted@brambles.org>
2024-04-15 03:01:03 +0100

    Start writing profile manager

--- src/biorand-re4r-web/src/routes/+page.svelte
+++ src/biorand-re4r-web/src/routes/+page.svelte
@@ -1,28 +1,5 @@
 <script>
-  import Card from '$lib/card.svelte';
+  import ProfileManager from '$lib/profileManager.svelte';
 </script>
 
-<div class="container my-3">
-  <div class="card">
-    <div class="card-body">
-      <div class="row">
-        <h3>Your randomizer is ready!</h3>
-        <p>Download the appropriate file and enjoy!</p>
-      </div>
-      <div class="row">
-        <Card title="Patch">
-          <span slot="text">Simply drop this file into your RE 4 install folder.</span>
-          <span slot="extra"
-            ><a id="btn-download-pak" href="#" class="btn btn-success">Download</a></span
-          >
-        </Card>
-        <Card title="Fluffy Mod">
-          <span slot="text">Add this to Fluffy Mod manager and enable it.</span>
-          <span slot="extra"
-            ><a id="btn-download-mod" href="#" class="btn btn-success">Download</a></span
-          >
-        </Card>
-      </div>
-    </div>
-  </div>
-</div>
+<ProfileManager />
