284a216017277dd5370a868df027140a8a836cb0
Ted John <ted@brambles.org>
2024-05-12 00:59:28 +0100

    Start writing about pages

--- src/biorand-re4r-web/src/routes/about/+page.svelte
+++ src/biorand-re4r-web/src/routes/about/+page.svelte
@@ -1,8 +1,45 @@
 <script>
-    import { Heading } from 'flowbite-svelte';
+    import { Heading, List, Li, A } from 'flowbite-svelte';
 </script>
 
-<div class="px-4">
-    <Heading>About</Heading>
-    <p>Placeholder page</p>
+<div class="px-4 mb-4">
+    <Heading tag="h2" class="mb-4">About</Heading>
+    <p class="mb-3">BioRand 4 is a cloud randomizer for CAPCOM's 2023 remake of Resident Evil 4.</p>
+    <p class="mb-3">
+        Randomizers can be configured and generated to improve replayability by enhancing the
+        gameplay. Some of the changes BioRand can make are:
+        <List class="m-4">
+            <Li>Randomize the starting inventory</Li>
+            <Li>Randomize the enemy types</Li>
+            <Li>Randomize the enemy health</Li>
+            <Li>Randomize the enemy item drops</Li>
+            <Li>Randomize the items found throughout the campaign</Li>
+            <Li>Randomize what the merchant sells</Li>
+            <Li>Randomize the price of items and upgrades</Li>
+            <Li>Add new enemy spawns</Li>
+        </List>
+    </p>
+    <p class="mb-3">
+        To generate a randomizer, sign up for an account and then login using your e-mail address
+        and user name. Once you have signed in, you will be able to configure a profile which
+        controls how BioRand randomizes the game. Once you are happy with your configuration, you
+        can then generate and download a new unique seed in the form of a .pak file or <A
+            href="https://github.com/fluffy-mods/ModManager">fluffy</A
+        > mod.
+    </p>
+
+    <Heading tag="h4" class="mb-4">Contributors</Heading>
+    <p class="mb-3">BioRand is created and developed by IntelOrca and the BioRand team.</p>
+    <List class="mb-3">
+        <Li>IntelOrca</Li>
+        <Li>AfkKun - Modding information and discovery</Li>
+        <Li>MightyKusKus - Game asset modification</Li>
+    </List>
+
+    <p class="mb-3">Special thanks to early access testers including but not limited to:</p>
+    <List>
+        <Li>404scamnotfound</Li>
+        <Li>DoubleEdger</Li>
+        <Li>Matthewsaurusrex</Li>
+    </List>
 </div>
--- src/biorand-re4r-web/src/routes/privacy/+page.svelte
+++ src/biorand-re4r-web/src/routes/privacy/+page.svelte
@@ -1,8 +1,93 @@
 <script>
-    import { Heading } from 'flowbite-svelte';
+    import { Heading, List, Li } from 'flowbite-svelte';
 </script>
 
-<div class="px-4">
-    <Heading>Privacy Policy</Heading>
-    <p>Placeholder page</p>
+<div class="px-4 mb-4">
+    <Heading tag="h2" class="mb-4">Privacy Policy</Heading>
+    <p>
+        This privacy policy sets out how BioRand uses and protects any information that you give
+        BioRand when you use this website.
+    </p>
+    <p>
+        BioRand is committed to ensuring that your privacy is protected. Should we ask you to
+        provide certain information by which you can be identified when using this website, then you
+        can be assured that it will only be used in accordance with this privacy statement.
+    </p>
+    <p>
+        BioRand may change this policy from time to time by updating this page. You should check
+        this page from time to time to ensure that you are happy with any changes. This policy is
+        effective from 1st May 2024.
+    </p>
+
+    <Heading tag="h4" class="mt-4 mb-2">What we collect</Heading>
+    <p>We collect the following information:</p>
+    <List>
+        <Li>e-mail address and online user name</Li>
+        <Li>custom randomizer profiles</Li>
+        <Li>site preferences and usage statistics</Li>
+    </List>
+
+    <Heading tag="h4" class="mt-4 mb-2">What we do with the information we gather</Heading>
+    <p>
+        We require this information to understand your needs and provide you with a better service,
+        and in particular for the following reasons:
+    </p>
+    <List>
+        <Li>We may use the information to improve our products and services.</Li>
+        <Li>
+            We may periodically send promotional emails about new products, special offers or other
+            information which we think you may find interesting using the email address which you
+            have provided.
+        </Li>
+        <Li>
+            From time to time, we may also use your information to contact you for market research
+            purposes. We may use the information to customise the website according to your
+            interests.
+        </Li>
+    </List>
+
+    <Heading tag="h4" class="mt-4 mb-2">Security</Heading>
+    <p>
+        We are committed to ensuring that your information is secure. In order to prevent
+        unauthorised access or disclosure, we have put in place suitable physical, electronic and
+        managerial procedures to safeguard and secure the information we collect online.
+    </p>
+
+    <Heading tag="h4" class="mt-4 mb-2">How we use cookies</Heading>
+    <p>
+        Cookies are not used for BioRand, however your browser local storage is used to temporarily
+        store your login session, unsaved profile, and application state.
+    </p>
+    <p>
+        The browser local storage is specific to BioRand and not shared with any third party
+        website.
+    </p>
+
+    <Heading tag="h4" class="mt-4 mb-2">Links to other websites</Heading>
+    <p>
+        Our website may contain links to other websites of interest. However, once you have used
+        these links to leave our site, you should note that we do not have any control over that
+        other website. Therefore, we cannot be responsible for the protection and privacy of any
+        information which you provide whilst visiting such sites and such sites are not governed by
+        this privacy statement. You should exercise caution and look at the privacy statement
+        applicable to the website in question.
+    </p>
+
+    <Heading tag="h4" class="mt-4 mb-2">Controlling your personal information</Heading>
+    <p>
+        We will not sell, distribute or lease your personal information to third parties unless we
+        have your permission or are required by law to do so. We may use your personal information
+        to send you promotional information about third parties which we think you may find
+        interesting if you tell us that you wish this to happen.
+    </p>
+    <p>
+        You may request details of personal information which we hold about you under the Data
+        Protection Act 1998. If you would like a copy of the information held on you, please note
+        that this can be seen by visiting your user profile page.
+    </p>
+    <p>
+        If you believe that any information we are holding on you is incorrect or incomplete, please
+        email us as soon as possible at support@biorand.net. We will promptly correct any
+        information found to be incorrect.
+    </p>
 </div>
