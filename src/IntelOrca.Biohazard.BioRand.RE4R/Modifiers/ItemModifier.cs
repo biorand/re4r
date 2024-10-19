440f436f674f8f22490e873410bd8062d49dd17d
Ted John <ted@brambles.org>
2024-10-19 16:33:32 +0100

    Update stack sizes for separate ways

--- src/IntelOrca.Biohazard.BioRand.RE4R/Modifiers/ItemModifier.cs
+++ src/IntelOrca.Biohazard.BioRand.RE4R/Modifiers/ItemModifier.cs
@@ -6,41 +6,34 @@ namespace IntelOrca.Biohazard.BioRand.RE4R.Modifiers
 {
     internal class ItemModifier : Modifier
     {
-        private const string ItemDefinitionUserDataPath = "natives/stm/_chainsaw/appsystem/ui/userdata/itemdefinitionuserdata.user.2";
+        private static string[] GetDataFiles(ChainsawRandomizer randomizer)
+        {
+            if (randomizer.Campaign == Campaign.Leon)
+            {
+                return
+                [
+                    "natives/stm/_chainsaw/appsystem/ui/userdata/itemdefinitionuserdata.user.2",
+                    "natives/stm/_chainsaw/appsystem/catalog/dlc/dlc_1401/itemdefinitionuserdata_dlc_1401.user.2",
+                    "natives/stm/_chainsaw/appsystem/catalog/dlc/dlc_1402/itemdefinitionuserdata_dlc_1402.user.2"
+                ];
+            }
+            else
+            {
+                return
+                [
+                    "natives/stm/_anotherorder/appsystem/ui/userdata/itemdefinitionuserdata_ao.user.2",
+                    "natives/stm/_anotherorder/appsystem/ui/userdata/itemdefinitionuserdata_ovr_ao.user.2"
+                ];
+            }
+        }
 
         public override void LogState(ChainsawRandomizer randomizer, RandomizerLogger logger)
         {
             var fileRepository = randomizer.FileRepository;
-            var itemDefinitions = fileRepository.GetUserFile(ItemDefinitionUserDataPath);
-            var datas = itemDefinitions.RSZ!.ObjectList[0].GetArray<RszInstance>("_Datas");
-            foreach (var data in datas)
+            foreach (var path in GetDataFiles(randomizer))
             {
-                var itemId = data.Get<int>("_ItemId");
-                var itemDefinition = ItemDefinitionRepository.Default.Find(itemId);
-                if (itemDefinition == null)
-                    continue;
-
-                if (!IsStackable(itemDefinition))
-                    continue;
-
-                var stackMax = IsWeapon(itemDefinition) ?
-                    data.Get("_WeaponDefineData._StackMax") :
-                    data.Get("_ItemDefineData._StackMax");
-
-                logger.LogLine($"{itemDefinition.Name}, stack = {stackMax}");
-            }
-        }
-
-        public override void Apply(ChainsawRandomizer randomizer, RandomizerLogger logger)
-        {
-            var stackMultiplier = randomizer.GetConfigOption<double>("inventory-stack-multiplier", 1);
-            if (stackMultiplier == 1)
-                return;
-
-            var fileRepository = randomizer.FileRepository;
-            fileRepository.ModifyUserFile(ItemDefinitionUserDataPath, (_, root) =>
-            {
-                var datas = root.GetArray<RszInstance>("_Datas");
+                var itemDefinitions = fileRepository.GetUserFile(path);
+                var datas = itemDefinitions.RSZ!.ObjectList[0].GetArray<RszInstance>("_Datas");
                 foreach (var data in datas)
                 {
                     var itemId = data.Get<int>("_ItemId");
@@ -51,15 +44,47 @@ namespace IntelOrca.Biohazard.BioRand.RE4R.Modifiers
                     if (!IsStackable(itemDefinition))
                         continue;
 
-                    var key = IsWeapon(itemDefinition) ?
-                        "_WeaponDefineData._StackMax" :
-                        "_ItemDefineData._StackMax";
+                    var stackMax = IsWeapon(itemDefinition) ?
+                        data.Get("_WeaponDefineData._StackMax") :
+                        data.Get("_ItemDefineData._StackMax");
 
-                    var stackMax = data.Get<int>(key);
-                    var newStackMax = Math.Clamp((int)Math.Round(stackMax * stackMultiplier), 1, 999);
-                    data.Set(key, newStackMax);
+                    logger.LogLine($"{itemDefinition.Name}, stack = {stackMax}");
                 }
-            });
+            }
+        }
+
+        public override void Apply(ChainsawRandomizer randomizer, RandomizerLogger logger)
+        {
+            var stackMultiplier = randomizer.GetConfigOption<double>("inventory-stack-multiplier", 1);
+            if (stackMultiplier == 1)
+                return;
+
+            var fileRepository = randomizer.FileRepository;
+            foreach (var path in GetDataFiles(randomizer))
+            {
+                fileRepository.ModifyUserFile(path, (_, root) =>
+                {
+                    var datas = root.GetArray<RszInstance>("_Datas");
+                    foreach (var data in datas)
+                    {
+                        var itemId = data.Get<int>("_ItemId");
+                        var itemDefinition = ItemDefinitionRepository.Default.Find(itemId);
+                        if (itemDefinition == null)
+                            continue;
+
+                        if (!IsStackable(itemDefinition))
+                            continue;
+
+                        var key = IsWeapon(itemDefinition) ?
+                            "_WeaponDefineData._StackMax" :
+                            "_ItemDefineData._StackMax";
+
+                        var stackMax = data.Get<int>(key);
+                        var newStackMax = Math.Clamp((int)Math.Round(stackMax * stackMultiplier), 1, 999);
+                        data.Set(key, newStackMax);
+                    }
+                });
+            }
         }
 
         private static bool IsStackable(ItemDefinition definition)
