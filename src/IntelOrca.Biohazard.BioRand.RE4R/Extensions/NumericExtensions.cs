e422236678b0e7279037fd82c7e7c348bb4865f8
Ted John <ted@brambles.org>
2024-11-06 19:59:29 +0000

    Add merchant to chapter 5

--- src/IntelOrca.Biohazard.BioRand.RE4R/Extensions/NumericExtensions.cs
+++ src/IntelOrca.Biohazard.BioRand.RE4R/Extensions/NumericExtensions.cs
@@ -1,6 +1,9 @@
-﻿namespace IntelOrca.Biohazard.BioRand.RE4R.Extensions
+﻿using System;
+using System.Numerics;
+
+namespace IntelOrca.Biohazard.BioRand.RE4R.Extensions
 {
-    internal static class NumericExtensions
+    public static class NumericExtensions
     {
         public static int RoundPrice(this double value)
         {
@@ -15,6 +18,23 @@
             if (value >= 10)
                 return (int)(value / 10) * 10;
             return (int)value;
+        }
+
+        public static Vector3 ToEuler(this Quaternion rotation)
+        {
+            var x = rotation.X;
+            var y = rotation.Y;
+            var z = rotation.Z;
+            var w = rotation.W;
+            var yaw = MathF.Atan2(2 * (y * w + x * z), 1 - 2 * (y * y + z * z));
+            var pitch = MathF.Asin(2 * (y * z - x * w));
+            var roll = MathF.Atan2(2 * (x * y + z * w), 1 - 2 * (x * x + y * y));
+            var yawDegrees = RadToDeg(yaw);
+            var pitchDegrees = RadToDeg(pitch);
+            var rollDegrees = RadToDeg(roll);
+            return new Vector3(yawDegrees, pitchDegrees, rollDegrees);
+
+            static float RadToDeg(float radians) => radians * (180f / MathF.PI);
         }
     }
 }
