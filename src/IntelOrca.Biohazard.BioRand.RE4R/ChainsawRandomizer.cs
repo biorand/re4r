30b19d7ff8b07eb85a7895229246998824595f21
Mark Parton <mightykuskus@gmail.com>
2025-10-18 12:41:30 +0100

    CraftWindowPatch
    Adding new patch to fix missing models on crafting window

--- src/IntelOrca.Biohazard.BioRand.RE4R/ChainsawRandomizer.cs
+++ src/IntelOrca.Biohazard.BioRand.RE4R/ChainsawRandomizer.cs
@@ -139,6 +139,7 @@ namespace IntelOrca.Biohazard.BioRand.RE4R
             // Patches
             new FlamethrowerPatch(this).Apply();
             new MendezPatch(this).Apply();
+            new CraftWindowPatch(this).Apply();
 
             // Apply modifiers
             IterateModifiers((n, m) =>
