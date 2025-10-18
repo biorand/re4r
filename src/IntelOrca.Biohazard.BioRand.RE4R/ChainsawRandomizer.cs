6a894cb7545407541103b89e68ae752274f2fbb3
Mark Parton <mightykuskus@gmail.com>
2025-10-18 12:48:37 +0100

    Removing CraftWindow patch
    Removing patch as breaks seed generation.

--- src/IntelOrca.Biohazard.BioRand.RE4R/ChainsawRandomizer.cs
+++ src/IntelOrca.Biohazard.BioRand.RE4R/ChainsawRandomizer.cs
@@ -139,7 +139,6 @@ namespace IntelOrca.Biohazard.BioRand.RE4R
             // Patches
             new FlamethrowerPatch(this).Apply();
             new MendezPatch(this).Apply();
-            new CraftWindowPatch(this).Apply();
 
             // Apply modifiers
             IterateModifiers((n, m) =>
