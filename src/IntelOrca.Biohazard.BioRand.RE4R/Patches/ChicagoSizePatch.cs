184b5fe796fc8dc133dcd9f56abed78e25e7c10c
Mark Parton <mightykuskus@gmail.com>
2025-12-30 16:16:57 +0000

    Reduce size of Chicago Typewriter to 2x6

---
+++ src/IntelOrca.Biohazard.BioRand.RE4R/Patches/ChicagoSizePatch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using IntelOrca.Biohazard.REE.Rsz;
+
+namespace IntelOrca.Biohazard.BioRand.RE4R.Patches
+{
+    internal class ChicagoSizePatch(IPatchContext context) : IPatch
+    {
+        public void Apply()
+        {
+
+            context.ModifyUserFile("natives/stm/_chainsaw/appsystem/ui/userdata/itemdefinitionuserdata.user.2", root =>
+            {
+                var datas = (RszArrayNode)root["_Datas"];
+                for (var i = 0; i < datas.Length; i++)
+                {
+                    var data = datas[i];
+                    if (data.Get<int>("_ItemId") == 275157056)
+                    {
+                        data = data
+                            .Set("_WeaponDefineData._ItemSize", 11);
+                        root = root.SetField("_Datas", datas.SetItem(i, data));
+                        break;
+                    }
+                }
+                return root;
+            });
+        }
+    }
+}
