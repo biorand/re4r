e1a86f132ba8f58a0ef17d124b4960664bf2be71
Mark Parton <mightykuskus@gmail.com>
2025-12-30 11:52:17 +0000

    Remove Around addition

--- src/IntelOrca.Biohazard.BioRand.RE4R/Patches/LegendaryWeaponsPatch.cs
+++ src/IntelOrca.Biohazard.BioRand.RE4R/Patches/LegendaryWeaponsPatch.cs
@@ -194,10 +194,10 @@ namespace IntelOrca.Biohazard.BioRand.RE4R.Patches
                         critAttachHash = (uint)MurMur3.HashData($"wp{id:0000}_f_c"); // Crit Hash that has an attachment
                     }
 
-                    var baseAroundHash = (uint)MurMur3.HashData($"wp{id:0000}Around"); // Base Hash
-                    var critAroundHash = (uint)MurMur3.HashData($"wp{id:0000}Around_c"); // Crit Hash
-                    var baseAroundAttachHash = (uint)MurMur3.HashData($"wp{id:0000}Around_f"); // Base Hash that has an attachment
-                    var critAroundAttachHash = (uint)MurMur3.HashData($"wp{id:0000}Around_f_c"); // Crit Hash that has an attachment
+                    var baseAroundHash = (uint)MurMur3.HashData($"wp{id:0000}"); // Base Hash
+                    var critAroundHash = (uint)MurMur3.HashData($"wp{id:0000}_c"); // Crit Hash
+                    var baseAroundAttachHash = (uint)MurMur3.HashData($"wp{id:0000}_f"); // Base Hash that has an attachment
+                    var critAroundAttachHash = (uint)MurMur3.HashData($"wp{id:0000}_f_c"); // Crit Hash that has an attachment
 
                     var attackDataList = (RszArrayNode)root["_AttackDataList"];
                     for (var i = 0; i < attackDataList.Length; i++)
