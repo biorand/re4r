6633210adcac3ae463c3c26361fc5175f70b3696
Ted John <tjohn@rocketsoftware.com>
2026-01-06 08:40:47 +0000

    Fix event gimmicks not working

--- src/IntelOrca.Biohazard.BioRand.RE4R/Modifiers/GimmickPlaceModifier.cs
+++ src/IntelOrca.Biohazard.BioRand.RE4R/Modifiers/GimmickPlaceModifier.cs
@@ -13,7 +13,7 @@ namespace IntelOrca.Biohazard.BioRand.RE4R.Modifiers
         {
             var gimmicks = randomizer.DynamicData.GetData(DynamicDataName.Gimmicks) ?? throw new Exception("Failed to get gimmick data");
             var placements = randomizer.GimmickService.GimmickPlacements
-                .Where(x => x.Campaign == randomizer.Campaign && x.Events.IsDefaultOrEmpty)
+                .Where(x => x.Campaign == randomizer.Campaign && x.Chapter != -1)
                 .ToImmutableArray();
 
             var rng = randomizer.GetRng("modifier/gimmickplace");
