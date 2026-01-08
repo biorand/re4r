8ffafdba916a5f04d2f5c66545ea517391b46380
Mark Parton <mightykuskus@gmail.com>
2026-01-08 16:27:32 +0000

    Fix Exclusives not displaying correctly and adding TypeofShoot as a changeable value

--- src/IntelOrca.Biohazard.BioRand.RE4R/Patches/LegendaryWeaponsPatch.cs
+++ src/IntelOrca.Biohazard.BioRand.RE4R/Patches/LegendaryWeaponsPatch.cs
@@ -80,6 +80,12 @@ namespace IntelOrca.Biohazard.BioRand.RE4R.Patches
             var weaponService = (context as FileRepository)?.Randomizer?.GetService<WeaponService>();
             weaponService?.RestrictUpgrades(id);
 
+            // Store exclusive message GUIDs
+            Guid excDescGuid1 = Guid.Empty;
+            Guid excPerkGuid1 = Guid.Empty;
+            Guid excDescGuid2 = Guid.Empty;
+            Guid excPerkGuid2 = Guid.Empty;
+
             UpdateMessages();
             UpdateShellInfo();
             UpdateBulletAttackHit();
@@ -108,15 +114,67 @@ namespace IntelOrca.Biohazard.BioRand.RE4R.Patches
                     var exPerk1 = (string)info["exclusive perk 1"];
                     var exDesc2 = (string)info["exclusive description 2"];
                     var exPerk2 = (string)info["exclusive perk 2"];
+                    
+                    // Generate new message entries for exclusive 1
                     if (!string.IsNullOrEmpty(exDesc1))
                     {
-                        msg.SetStringAll($"CH_Mes_Main_WpCustom_{id:0000}_02_00", exDesc1);
-                        msg.SetStringAll($"CH_Mes_Main_WpCustom_{id:0000}_02_01", exPerk1);
+                        var exc1DescName = $"CH_Mes_Main_WpCustom_{id:0000}_Legendary_00";
+                        var exc1PerkName = $"CH_Mes_Main_WpCustom_{id:0000}_Legendary_01";
+                        
+                        var descMsg = msg.FindMessage(exc1DescName);
+                        if (descMsg == null)
+                        {
+                            var newDesc = msg.Create(exc1DescName, exDesc1);
+                            excDescGuid1 = newDesc.Guid;
+                        }
+                        else
+                        {
+                            excDescGuid1 = descMsg.Guid;
+                            msg.SetStringAll(exc1DescName, exDesc1);
+                        }
+                        
+                        var perkMsg = msg.FindMessage(exc1PerkName);
+                        if (perkMsg == null)
+                        {
+                            var newPerk = msg.Create(exc1PerkName, exPerk1);
+                            excPerkGuid1 = newPerk.Guid;
+                        }
+                        else
+                        {
+                            excPerkGuid1 = perkMsg.Guid;
+                            msg.SetStringAll(exc1PerkName, exPerk1);
+                        }
                     }
+                    
+                    // Generate new message entries for exclusive 2
                     if (!string.IsNullOrEmpty(exDesc2))
                     {
-                        msg.SetStringAll($"CH_Mes_Main_WpCustom_{id:0000}_02_02", exDesc2);
-                        msg.SetStringAll($"CH_Mes_Main_WpCustom_{id:0000}_02_03", exPerk2);
+                        var exc2DescName = $"CH_Mes_Main_WpCustom_{id:0000}_Legendary_02";
+                        var exc2PerkName = $"CH_Mes_Main_WpCustom_{id:0000}_Legendary_03";
+                        
+                        var descMsg = msg.FindMessage(exc2DescName);
+                        if (descMsg == null)
+                        {
+                            var newDesc = msg.Create(exc2DescName, exDesc2);
+                            excDescGuid2 = newDesc.Guid;
+                        }
+                        else
+                        {
+                            excDescGuid2 = descMsg.Guid;
+                            msg.SetStringAll(exc2DescName, exDesc2);
+                        }
+                        
+                        var perkMsg = msg.FindMessage(exc2PerkName);
+                        if (perkMsg == null)
+                        {
+                            var newPerk = msg.Create(exc2PerkName, exPerk2);
+                            excPerkGuid2 = newPerk.Guid;
+                        }
+                        else
+                        {
+                            excPerkGuid2 = perkMsg.Guid;
+                            msg.SetStringAll(exc2PerkName, exPerk2);
+                        }
                     }
                 });
             }
@@ -322,6 +380,10 @@ namespace IntelOrca.Biohazard.BioRand.RE4R.Patches
                                 {
                                     data = data.Set("_WeaponStructureParam._ReloadSpeedRate", baseReloadSpeed);
                                 }
+                                if (info["typeofshoot"] is int typeOfShoot)
+                                {
+                                    data = data.Set("_WeaponStructureParam.TypeOfShoot", new[] { typeOfShoot });
+                                }
                                 root = root.SetField("_DataTable", datas.SetItem(i, data));
                                 break;
                             }
@@ -411,50 +473,11 @@ namespace IntelOrca.Biohazard.BioRand.RE4R.Patches
 
                 void UpdateCustom()
                 {
-                    var zeroGuid = Guid.Empty;
-                    var weaponGuidMap = new Dictionary<int, (Guid NameGuid, Guid DescGuid, Guid PerkGuid, Guid ExcDescGuid1, Guid ExcPerkGuid1, Guid ExcDescGuid2, Guid ExcPerkGuid2)>
-                    {
-                        [4000] = (new Guid("03ccf4c8-8909-4bc0-b797-5ac75191eb34"), new Guid("d487ea63-4a0f-42f7-acc6-3fda455b59bd"), new Guid("a66feb97-34f1-45f5-a3b7-682a5707e066"),
-                            new Guid("02af5c23-8b43-443b-ab97-e07248208856"), new Guid("b3d6c88c-dfc8-42c8-8e65-65439dbe8045"), zeroGuid, zeroGuid), //Exclusive 1
-                        [4001] = (new Guid("e2c3e5ec-e29c-430f-a25a-20e9d1d9d1b2"), new Guid("d67eb859-772f-464b-b950-78a4d7bbb1a3"), new Guid("9f851e2f-32ec-4170-9a14-e6c1b0622b33"),
-                            new Guid("fad103a5-e4c6-4f96-bb68-9cf43104a7f3"), new Guid("d97663df-5a70-406a-88cb-d20d4b8d1593"), zeroGuid, zeroGuid), //Exclusive 1
-                        [4002] = (new Guid("7013eb44-3c6b-44d0-9efd-6141df373f9c"), new Guid("09045a80-04cc-4851-8493-227cb6d9713d"), new Guid("4209a643-1cd0-4873-9f11-2c57c9ce5c5b"),
-                            new Guid("507ff7c7-838a-43fc-b7e9-ad543fa154e6"), new Guid("ee0c0332-4263-42bb-8cd8-934fcd15aab6"), zeroGuid, zeroGuid), //Exclusive 1
-                        [4003] = (new Guid("a278cbeb-a0fc-427d-9b68-27b37048cd22"), new Guid("5351200d-9143-4f44-b960-f71edf6acc42"), new Guid("4a4b8212-f4d2-46f5-b7fc-ba5d1727740f"),
-                            new Guid("c56604cc-1957-42b2-b017-be3de567e6ef"), new Guid("b6c5ddbb-62a4-4572-95d2-38f472520217"), zeroGuid, zeroGuid), //Exclusive 1
-                        [4004] = (new Guid("57ae58d8-c898-4431-b1a8-164028aad43c"), new Guid("b77736de-212f-46e0-adfc-44c03a9d3087"), new Guid("54598876-8928-40cd-b823-5f94373a3c95"),
-                            new Guid("54f41279-a6c3-4bde-a240-99069212451e"), new Guid("50b88315-19b4-4a99-bb69-dd876ff2efed"), //Exclusive 1
-                            new Guid("e15d5b34-2317-4278-8894-159a0a829ca3"), new Guid("afea82ca-6dee-4737-8e50-6125bef8b3fd")), //Exclusive 2
-                        [4100] = (new Guid("02d5ce0c-1fff-4ef4-9c30-0e9d11c5cfeb"), new Guid("09206c08-adb7-44d5-81db-3c654eeacf15"), new Guid("51221448-1a6c-4fb6-9b7e-3d6171c73fbc"),
-                            new Guid("0ea5e426-4757-4ca0-b1b1-8c3c67060806"), new Guid("2d510d09-66d4-4256-8e2d-d81908d15db3"), zeroGuid, zeroGuid), //Exclusive 1
-                        [4101] = (new Guid("5b60b9c3-3ad6-4327-ad45-7575d7c6e4c2"), new Guid("8beb6da9-599f-4243-8e51-e5e3f0c7c8e1"), new Guid("1226e92d-7559-418d-a561-4f0c477927c9"),
-                            new Guid("172e798d-15cc-4399-9f36-3bc86cd2a669"), new Guid("c878fc3b-16d3-42c6-9726-7dc2e43383f5"), zeroGuid, zeroGuid), //Exclusive 1
-                        [4102] = (new Guid("39841abc-fe10-4ac8-ac6a-e838a5faf93f"), new Guid("cf98c0c9-2540-4495-a40a-dea0ab5b8d2a"), new Guid("f915b302-a024-4208-a6a4-ab599390eb66"),
-                            new Guid("d63edecc-c48e-41b0-a22c-919c086417e1"), new Guid("9b60cce3-80bf-4e87-9b04-234defcbd0f6"), zeroGuid, zeroGuid), //Exclusive 1
-                        [4200] = (new Guid("80413c24-3dfe-40ee-9ad1-b9cebd8a01f0"), new Guid("bc6e4529-034f-4d21-a93b-fab23a06bcd9"), new Guid("8814353c-a011-427a-b25f-df0e4f921aa0"),
-                            new Guid("5933edbb-f6c2-461c-bca2-a90c59ad0464"), new Guid("48a93d0c-1d18-471b-b084-15a396620726"), zeroGuid, zeroGuid), //Exclusive 1
-                        [4201] = (new Guid("f091de88-316a-4da4-bae4-3468141fc7af"), new Guid("4644300e-7ebe-4342-ae96-2410f071d904"), new Guid("00e1388d-c355-489c-a87f-9ef25278f283"),
-                            new Guid("11b51fbe-4a84-446f-ada4-e57383083b16"), new Guid("e0e7066d-ff54-46f7-a7c4-ca75d3049498"), zeroGuid, zeroGuid), //Exclusive 1
-                        [4202] = (new Guid("2504d632-47a7-4ac6-910a-6c28aa370fe6"), new Guid("5f4af552-01b6-4f96-9de0-f9934dfb2f3f"), new Guid("03532c0f-252e-440c-b369-953a57d7302c"),
-                            new Guid("5e39af43-9116-4a53-9878-ab657c7889d4"), new Guid("f25416cc-d768-4538-be0d-eec6c3c7029d"), zeroGuid, zeroGuid), //Exclusive 1
-                        [4400] = (new Guid("177bc20b-3329-41ca-9647-2da44d898652"), new Guid("66283ff3-bc3a-42a7-9392-b0f7f60e4098"), new Guid("82c49d84-ea0b-4349-9385-87abf822505f"),
-                            new Guid("75664c21-82ef-4fcc-87ed-08851eeff67a"), new Guid("8743ff78-41ba-4132-9827-3850d67bb2f3"), zeroGuid, zeroGuid), //Exclusive 1
-                        [4401] = (new Guid("b0987256-cbcd-4e4e-b4f7-802c83eb263e"), new Guid("9634589d-9b6d-4475-9ca5-f2943cb2cdc8"), new Guid("0b54a4a0-4065-4ad4-a0fe-975315d18cc1"),
-                            new Guid("686fb216-944a-4b2f-9985-f6c993cb17a4"), new Guid("9d7ce4f2-4594-4a21-9db1-abf4b7e8de6b"), zeroGuid, zeroGuid), //Exclusive 1
-                        [4402] = (new Guid("16db480e-5c93-4934-a8ed-61da0991e0fd"), new Guid("c8ce3e84-26a8-4ade-a892-78155358f1c2"), new Guid("552ddcb8-1932-4943-b997-e7b9fbc5b210"),
-                            new Guid("bdd0ed9b-1587-4b4e-ab39-d8fab8d7e51a"), new Guid("179e5a08-b251-49ae-8638-c7f96d3ec24f"), zeroGuid, zeroGuid), //Exclusive 1
-                        [4500] = (new Guid("f8de7263-bf92-4ab3-b624-daa40be1ee23"), new Guid("1d29d246-6c26-46ab-a8c7-f79d8695e0f6"), new Guid("dde3e8af-28cc-4917-a4b5-25b239eec942"),
-                            new Guid("ec3bdfd4-5276-40ec-b0ea-8ec208e3862a"), new Guid("0a531c93-b694-4291-a2a9-210d07413c8e"), zeroGuid, zeroGuid), //Exclusive 1
-                        [4501] = (new Guid("27d4165f-a189-4c6b-a66c-0cb1ef45b5b4"), new Guid("f2d6a5cb-225f-4a54-9c44-0db5508a739a"), new Guid("e34c10c0-9d17-492b-bfc3-2f446e97db5e"),
-                            new Guid("c29eba77-2eb7-4551-b6c3-f82af49c839b"), new Guid("840d6098-cc66-482d-934a-3008b8844778"), zeroGuid, zeroGuid), //Exclusive 1
-                        [4502] = (new Guid("4362e42c-33f4-4c35-b309-092f69edea3a"), new Guid("c97f06a7-ab32-41e8-ab06-3f0fd14ea56b"), new Guid("6ec2cd54-cff5-47fa-9963-c01bb9dfccb8"),
-                            new Guid("d4c92c10-966c-44c5-b72a-8f8a607db993"), new Guid("ed14e9d0-1628-4f55-bf6a-1c98caa1278f"), zeroGuid, zeroGuid), //Exclusive 1
-                    };
-
                     context.ModifyUserFile("natives/stm/_chainsaw/appsystem/weaponcustom/weaponcustomuserdata.user.2", root =>
                     {
                         var userdata = RszSerializer.Deserialize<chainsaw.WeaponCustomUserdata>(root)!;
                         var stage = userdata._WeaponStages.First(x => x._WeaponID == id);
+                        
                         //adding all info and cost values for damage
                         for (var i = 0; i < 5; i++)
                         {
@@ -668,24 +691,20 @@ namespace IntelOrca.Biohazard.BioRand.RE4R.Patches
                             var limitBreak = limitBreaks[i];
                             limitBreak._LimitBreakCustomCategory = limitDataMap[key];
 
-                            // Set message IDs if available
-                            if (weaponGuidMap.TryGetValue(id, out var guids))
+                            // Set message IDs
+                            if (i == 0 && excDescGuid1 != Guid.Empty)
                             {
-                                if (i == 0)
-                                {
-                                    limitBreak._CustomLimitBreak._MessageId = guids.ExcDescGuid1;
-                                    limitBreak._CustomLimitBreak._PerksMessageId = guids.ExcPerkGuid1;
-                                }
-                                else if (i == 1)
-                                {
-                                    limitBreak._CustomLimitBreak._MessageId = guids.ExcDescGuid2;
-                                    limitBreak._CustomLimitBreak._PerksMessageId = guids.ExcPerkGuid2;
-                                }
+                                limitBreak._CustomLimitBreak._MessageId = excDescGuid1;
+                                limitBreak._CustomLimitBreak._PerksMessageId = excPerkGuid1;
                             }
-                            // Set rate value
+                            else if (i == 1 && excDescGuid2 != Guid.Empty)
+                            {
+                                limitBreak._CustomLimitBreak._MessageId = excDescGuid2;
+                                limitBreak._CustomLimitBreak._PerksMessageId = excPerkGuid2;
+                            }
+                            
                             limitBreak._CustomLimitBreak._RateValue = Convert.ToSingle(info[$"{key} exclusive"]);
 
-                            // Set cost if present and valid
                             var costKey = $"{key} cost exclusive";
                             if (info[costKey] is int costExclusive)
                             {
