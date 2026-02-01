e5cffd4a5f2835c2bab805e8657ad154b4ea7634
Ted John <ted@brambles.org>
2026-02-01 18:00:05 +0000

    Separate out biorand title graphic to patch

---
+++ src/IntelOrca.Biohazard.BioRand.RE4R/Patches/BiorandTitlePatch.cs
@@ -0,0 +1,14 @@
+﻿namespace IntelOrca.Biohazard.BioRand.RE4R.Patches
+{
+    /// <summary>
+    /// Changes the title screen logo from RESIDENT EVIL/BIOHAZARD 4 to BIORAND.
+    /// </summary>
+    /// <param name="context"></param>
+    internal class BiorandTitlePatch(IPatchContext context) : IPatch
+    {
+        public void Apply()
+        {
+            context.ApplyOverlay(context.GetSupplementFile("biorand_title.zip")!);
+        }
+    }
+}
