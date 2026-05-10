266d67e1b3652bfb121a46858d3dd0c409b41af3
Ted John <ted@brambles.org>
2026-05-10 11:36:21 +0100

    Update entry point project

---
+++ src/IntelOrca.Biohazard.BioRand.RE4R/Re4rRandomizerApp.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+
+namespace IntelOrca.Biohazard.BioRand.RE4R
+{
+    public class Re4rRandomizerApp
+    {
+        public int Run(string[] args)
+        {
+            var randomizerApp = new ReeRandomizerApp();
+            randomizerApp.ApplicationName = "biorand-re4r";
+            randomizerApp.ApplicationVersion = VersionHelper.GetGitHashShort(Assembly.GetExecutingAssembly());
+            randomizerApp.ExamplePakFilename = "re_chunk_000.pak.patch_007.pak";
+            randomizerApp.ExampleInstallPath = @"C:\Program Files (x86)\Steam\steamapps\common\RESIDENT EVIL 4  BIOHAZARD RE4";
+            randomizerApp.Randomizer = new Re4rRandomizer();
+            return randomizerApp.Run(args);
+        }
+    }
+}
